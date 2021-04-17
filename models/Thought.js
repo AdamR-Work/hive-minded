e1598a07898a368c72aa0d076de13cc1b80345b5
AdamR-Work <aramosatwork@hotmail.com>
2021-04-17 18:19:56 -0500

    models and base folder structure in place

---
+++ models/Thought.js
@@ -0,0 +1,67 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+// Thought Model for people to express themselves
+const ThoughtSchema = new Schema(
+{
+    thoughtText: {
+      type: String,
+      required: true,
+      min:[1,"It seems to be blank. No thoughts on this?"],
+      max:[280, "Hmmm a bit wordy, try to consoldiate your thoughts."]
+
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions:[ReactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+// Reaction SCHEMA attached to the Thought Model used to help people respond to their friends thoughts.  
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      max:[280, "Its good you feel so strongly about this Comment but lets try and be nice. Keep it Short and Sweet."]
+    },
+    username: {
+      type: String,
+      required:true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+ReactionSchema.virtual('reactionCount').get(function() {
+  return this.replies.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
